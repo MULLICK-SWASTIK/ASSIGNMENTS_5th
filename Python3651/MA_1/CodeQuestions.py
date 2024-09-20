35a4a923873a179186e31eba301ff6e5230bc87c
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-09-20 20:05:19 +0530

    Added missing files

---
+++ Python3651/MA_1/CodeQuestions.py
@@ -0,0 +1,86 @@
+# Question 7
+# a = int(input("Enter a:"))
+# a = 20
+# b = 10
+# print(a + b)
+# '''OUTPUT
+# Enter a:2
+# 30
+# '''
+
+
+# Question 8
+# a = "nana"
+# b = 8
+# c = a * b
+# print(c)
+# '''OUTPUT
+# nananananananananananananananana'''
+
+
+# Question 9
+# print("Hi! We are studying \"Python\".\\n I hope you all are doing well.\nWe are going to have a great time!")
+# '''OUTPUT
+# Hi! We are studying "Python".\n I hope you all are doing well.
+# We are going to have a great time!'''
+
+
+# Question 11 P(x)= x^3 + 2x^2 + 3x + 4
+# import math
+
+# x = int(input("Enter value for x: "))
+# px = (math.pow(x, 3)) + 2 * (math.pow(x, 2)) + 3 * x + 4
+# print(f"P({x}) = {px}")
+# """OUTPUT
+# Enter value for x: 2
+# P(2) = 26.0"""
+
+
+# Question 12
+# import math
+
+# radius = float(input("Enter radius of a circle: "))
+# cir_area = math.pi * math.pow(radius, 2)
+# vol_sphere = (4 / 3) * (math.pi * math.pow(radius, 3))
+# print(
+#     "Area of the circle is %.2f and volume of sphere is %.2f for radius %.2f"
+#     % (cir_area, vol_sphere, radius)
+# )
+# '''OUTPUT
+# Enter radius of a circle: 5.6
+# Area of the circle is 98.52 and volume of sphere is 735.62 for radius 5.60'''
+
+
+# Question 13
+# n = int(input("Enter a value for n: "))
+# cumulate = (n * (n + 1)) / 2
+# print(f"Sum of integers from 1 to {n} is {int(cumulate)}")
+# '''OUTPUT
+# Enter a value for n: 10
+# Sum of integers from 1 to 10 is 55'''
+
+
+# Question 14
+# a, b, c = 10, 5, 0 # True
+# a, b, c = 1.21, 1.20, 1.22 #False
+# cond = (a < b) or (not (c == b) and (c < a))
+# print(cond)
+
+
+# Question 17
+# # a.
+# print(-7 * 20 + 8 / 16 * 2 + 54)  # -85.0
+# # b.
+# print(7**2 // 9 % 3)  # 2
+# # c.
+# print((7 - 4 * 2) * 10 - 25 * 8 // 5)  # -50
+# # d.
+# print(5 % 10 + 10 - 25 * 8 // 5) # -25
+
+
+
+
+#Question 19
+mealCost=float(input("Enter cost of meal: "))
+
+

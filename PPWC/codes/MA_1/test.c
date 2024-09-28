a24d0c0910b96d54961f9c95e8ef1867343f5eb1
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-09-28 21:00:48 +0530

    Modified PPWC\codes\MA_1"

--- PPWC/codes/MA_1/test.c
+++ PPWC/codes/MA_1/test.c
@@ -1,10 +1,54 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
+// int main(void)
+// {
+//     printf("Enter some value: ");
+//     int a, b;
+//     int res = scanf("%d %d", &a, &b);
+//     printf("Result: %d \n%d %d", res, a, b);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+//     double x = 3000.0, y = 0.0035;
+//     printf("%f %f %f\n", x, y, x * y, x / y);
+//     printf("%e %e %e\n", x, y, x * y, x / y);
+//     printf("%E %E %E\n", x, y, x * y, x / y);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 54321;
+//     float x = 876.543;
+//     printf(":%3d: :%5d: :%10d: :%12d:\n", i, i, i, i);
+//     printf(":%3f: :%10f: :%13f: :%f:\n", x, x, x, x);
+//     return 0;
+// }
+#include <stdio.h>
+int main()
 {
-    printf("Enter some value: ");
-    int a, b;
-    int res = scanf("%d %d", &a, &b);
-    printf("Result: %d \n%d %d", res, a, b);
+    int a, b, c;
+    printf("Enter in decimal format: ");
+    scanf("%d", &a);
+    getchar();
+    getchar();
+    printf("Enter in octal format: ");
+    scanf("%d", &b);
+    getchar();
+    printf("Enter in hexadecimal format: ");
+    scanf("%d", &c);
+    getchar();
+    printf("a = %d, b = %d, c = %d", a, b, c);
+    printf("Enter in decimal format: ");
+    scanf("%i", &b);
+    getchar();
+    printf("Enter in octal format: ");
+    scanf("%i", &b);
+    getchar();
+    printf("Enter in hexadecimal format: ");
+    scanf("%i", &c);
+    printf("a = %i, b = %i, c = %i\n", a, b, c);
     return 0;
 }

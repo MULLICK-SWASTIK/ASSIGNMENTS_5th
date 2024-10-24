55155619a410aaf59a5307d4e4c976835aba658f
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-10-24 18:07:11 +0530

    Added MA_2: Q7.c and Q7.exe (code and executable files)

---
+++ PPWC/codes/MA_2/Q7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int mark;
+    printf("Enter your marks: ");
+    scanf("%d", &mark);
+    switch (mark)
+    {
+    case 95 ... 100:
+        printf("Grade: O\n");
+        break;
+    case 81 ... 94:
+        printf("Grade: A\n");
+        break;
+    case 71 ... 80:
+        printf("Grade: B\n");
+        break;
+    case 61 ... 70:
+        printf("Grade: C\n");
+        break;
+    case 51 ... 60:
+        printf("Grade: D\n");
+        break;
+    case 40 ... 50:
+        printf("Grade: E\n");
+        break;
+    case 0 ... 39:
+        printf("Grade: F\n");
+        break;
+    default:
+        printf("Invalid mark!\n");
+        break;
+    }
+}
+
+/*
+OUTPUT
+Enter your marks: 56
+Grade: D
+*/
